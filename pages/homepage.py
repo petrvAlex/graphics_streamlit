1d572a5ac26d7be8fd52150246ff39882880ab92
aleksandrpetrov <petrv.alex@outlook.com>
2025-11-21 02:22:57 +0300

    Первая попытка мультистраничного приложения

--- pages/homepage.py
+++ pages/homepage.py
@@ -1,13 +1,7 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-)
 
 st.write("# Welcome to Streamlit! 👋")
-
-st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
